ee8c662c509d7889da743bf53b0513928cb4fe74
Mandarin Drummond <mandarind@gmail.com>
2013-07-30 12:36:20 -0400

    Better graphic

--- scripts/apps/api/flows/splash.js
+++ scripts/apps/api/flows/splash.js
@@ -13,8 +13,8 @@ define([
             var self = this,
                 block = function(num) {
                 self.blocks.push($(document.createElement('div'))
+                    .append('*')
                     .addClass("splash-block splash-block-" + num)
-                    .css("width", (self.blocks.length + 1) * 10)
                     .appendTo(self.$el));
             };
             this.$el.empty();
@@ -30,7 +30,7 @@ define([
                 block.hide().fadeIn(index * 2 * 500);
             });
         })
-        .delay(5000)
+        .delay(10000)
         .step("three", function() {
             _.each(this.blocks, function(block, index) {
                 block.fadeOut(1000/(index+1), function() {
@@ -38,7 +38,7 @@ define([
                 });
             });
         })
-        .delay(2000);
+        .delay(8000);
 
 
     return flow;
